ab2305a82fa68a7fe10f711beae5f187b564a75d
Hua Yan <hyan@aum.edu>
2024-11-06 08:51:12 -0600

    Update p01beg_3gui_tkinter.py
    imcomplete

--- p01_sales/p01sc06_OOPDBGUI3tier/p01beg_3gui_tkinter.py
+++ p01_sales/p01sc06_OOPDBGUI3tier/p01beg_3gui_tkinter.py
@@ -1,1 +1,18 @@
+# [import any other necessary module(s)]
 
+from datetime import datetime
+
+
+class SalesFrame(ttk.Frame):
+    '''Implement any necessary methods'''
+    
+
+def main():
+    root = tk.Tk()
+    root.title("Edit Sales Amount")
+    SalesFrame(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
